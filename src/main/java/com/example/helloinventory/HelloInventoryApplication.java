fdcd025bf4da5d65651a7cbc05fc6d2b61d76900
leejinwoo <leejinwoo1126@gmail.com>
2024-10-01 19:38:32 +0900

    init

---
+++ src/main/java/com/example/helloinventory/HelloInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/helloinventory/HelloInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
