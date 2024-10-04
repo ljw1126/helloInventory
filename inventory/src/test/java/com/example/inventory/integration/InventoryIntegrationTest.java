75821d18f02c96782310580cbbda375d21a64b86
leejinwoo <leejinwoo1126@gmail.com>
2024-10-04 10:42:51 +0900

    test: SpringBootTest 골격 생성

---
+++ inventory/src/test/java/com/example/inventory/integration/InventoryIntegrationTest.java
@@ -0,0 +1,50 @@
+package com.example.inventory.integration;
+
+import com.example.inventory.test.exception.NotImplementedException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class InventoryIntegrationTest {
+
+    @DisplayName("재고 조회 실패")
+    @Test
+    void test() {
+        throw new NotImplementedException();
+    }
+
+    @DisplayName("재고 조회 성공")
+    @Test
+    void test2() {
+        throw new NotImplementedException();
+    }
+
+    @DisplayName("재고 차감 실패")
+    @Test
+    void test3() {
+        throw new NotImplementedException();
+    }
+
+    @DisplayName("재고 차감 성공")
+    @Test
+    void test4() {
+        throw new NotImplementedException();
+    }
+
+    @DisplayName("재고 수정 실패")
+    @Test
+    void test5() {
+        throw new NotImplementedException();
+    }
+
+    @DisplayName("재고 수정 성공")
+    @Test
+    void test6() {
+        throw new NotImplementedException();
+    }
+
+    @DisplayName("재고 차감, 수정 종합")
+    @Test
+    void test7() {
+        throw new NotImplementedException();
+    }
+}
