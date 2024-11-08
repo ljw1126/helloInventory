293bb8f907f8ce66532688ef2cdd1a315f7a2cc6
leejinwoo <leejinwoo1126@gmail.com>
2024-11-08 20:24:22 +0900

    feat: jacoco-conventions.gradle.kts 추가

--- buildSrc/src/main/kotlin/EnvUtils.kt
+++ buildSrc/src/main/kotlin/EnvUtils.kt
@@ -2,4 +2,12 @@ object EnvUtils {
     fun isAct():Boolean { // ACT 환경인 경우 true
         return System.getenv("ACT") != null
     }
+
+    fun isLocal(): Boolean {
+        return System.getenv("CI") == null
+    }
+
+    fun isCI(): Boolean {
+        return System.getenv("CI") != null
+    }
 }
